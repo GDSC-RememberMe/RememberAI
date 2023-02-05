6f610bcfe740ecc701db54bf6c4ebcd7035d8bb8
paragonyun <paragonyun@naver.com>
2023-02-05 23:39:15 +0900

    model

--- EventClassifier/model.py
+++ EventClassifier/model.py
@@ -5,21 +5,18 @@ from utils import train_augs
 from keras.applications.inception_v3 import InceptionV3
 from keras.models import Model
 
-class EventRecognizer(keras.Model):
+class EventRecognizer(tf.keras.Model):
     def __init__(self, **kwargs):
-        self.base_model = InceptionV3(include_top=False)
+        super(EventRecognizer, self).__init__()
+        self.base_model = InceptionV3(include_top=False, input_shape=(256, 256, 3))
         self.model = Model(inputs=self.base_model.input, outputs=self.base_model.output)
-
         self.aug_layer = train_augs
+        self.fc = tf.keras.layers.Dense(19)
+        
+        self.build(input_shape=(256,256,3))
 
     def call(self, x):
-        x = train_augs(x)
+        x = self.aug_layer(x)
         x = self.model(x)
-        return x
-
-from glob import glob
-datas = glob("./data/*")
-
-x = EventRecognizer(datas[0])
-print(x.shape)
-    
+        output = self.fc(x)
+        return output
