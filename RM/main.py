713e775a2e2ce7231486b267a082b9e656c7e356
paragonyun <paragonyun@naver.com>
2023-03-15 21:25:39 +0900

    feat: post

--- RM/main.py
+++ RM/main.py
@@ -1,18 +1,24 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, HTTPException
 from keyword_extractor import *
 from pydantic import BaseModel
 
 app = FastAPI()
 
 class InputText(BaseModel):
-    text: str
+    contents: str
 
-@app.post("/keyword")
+
+@app.post("/keyword/")
 async def keywords(text: InputText):
     input_txt = text.dict()
-
-    result = keyword(sentence=input_txt['contents'])
+    
+    result = keyword(doc=input_txt['contents'])
 
     data = {"keyword" : result}
 
     return data
+
+# @app.exception_handler(Exception)
+# async def exception_handler(request, exc):
+#     print(repr(exc)) # 예외 객체 출력
+#     return {"detail": "Internal server error"} # 클라이언트에 반환할 메시지
