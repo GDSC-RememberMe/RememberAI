13cd9e62321d10061bce706f9d5b9607c0a96cba
paragonyun <paragonyun@naver.com>
2023-03-17 13:59:30 +0900

    get category and keyword

--- RM/doc_cluster.py
+++ RM/doc_cluster.py
@@ -1,33 +1,48 @@
 from sentence_transformers import SentenceTransformer
-from sklearn.cluster import DBSCAN, KMeans
-import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+from keyword_extractor import *
 
 model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+cat_lst = ["결혼", "물놀이", "졸업", "명절", "일상", "요리", "운동", "등산", "생일", "식사"]
 
+def tokenizer(sentence):
+    mecab = Mecab(dicpath=r"C:\mecab\mecab-ko-dic")
+    pos_lst = ["NNG", "NNP"]
+    filtered_lst = [word for word, pos in mecab.pos(sentence) if pos in pos_lst]
 
-def doc_cluster(evt_lst):
-    embds = model.encode(evt_lst)
-    # print(embds.shape) (701, 768)
-    # dbscan = DBSCAN()
-    # labels = dbscan.fit_predict(embds)
+    return filtered_lst
 
-    kmeans = KMeans(n_clusters=5)
-    labels = kmeans.fit_predict(embds)
+def get_cat_key(sentence, cat_lst=cat_lst):
 
-    return_df = pd.DataFrame({"sentence" : evt_lst,
-                                "label" : labels})
+    sentence_emb = model.encode([sentence])
+    cat_emb = model.encode(cat_lst)
+    distance = cosine_similarity(sentence_emb, cat_emb)
+    category = cat_lst[distance.argsort()[0][-1]]
+
+    kw_model = KeyBERT(model=model)
     
-    return return_df
+    keywords = kw_model.extract_keywords(sentence, seed_keywords=[category])
+
+    candidates_lst = [word for word, _ in keywords]
+    candidates_lst = " ".join(candidates_lst)
+
+    fin_keyword = tokenizer(candidates_lst)[0]
+
+    return category, fin_keyword
+
 
 if __name__ == "__main__":
-    with open("./oldsentence.txt", "r", encoding='UTF8') as f:
-        lines = f.readlines()
-        sentences = [sentence[:-1] for sentence in lines]
-    
-    df = doc_cluster(evt_lst=sentences)
-    print(len(df['label'].unique()))
-    for lab in df['label'].unique():
-        print(f"Clutser {lab}")
-        
-        print(df[df['label'] == lab]['sentence'][:10])
+    test1 = "추석에 온 가족이 모여 찍은 가족사진. 오랜만에 모여 윳놀이도 하고, 고스톱도 치고 재미있었다."
+    test2 = "강릉여행. 온 가족이 함께 강릉으로 여행을 갔다. 다같이 가서 회도 먹고 물놀이도 하고 너무 즐거웠다"
+    test3 = "진통이 정말 심했어요. 새벽에 아버지한테 제가 전화해서 너무 아프다고 울었어요. 그래서 그날 가족들 다같이 병원으로 와서 진통하는 걸 봤었지요. 그때 아머지가 계속 손잡고 옆을 지켜주셨어요. 둘째 낳고 아버지가 보양식이며 배냇저고리며 이것저것 엄청 챙겨주셨던 기억이 나요. 낳자마자 아버지가 손수 이름 지어주겠다고 사흘밤낮동안 고민하셨던 것도 눈에 선해요"
+
+    test_lst = [test1, test2, test3]
+    for sentence in test_lst:
+        print("Sentence")
+        print(sentence)
+        category, keyword = get_cat_key(sentence=sentence,
+                                        cat_lst=cat_lst)
+        print("-> Category: ", category)
+        print("-> Keyword: ", keyword)
         print()
